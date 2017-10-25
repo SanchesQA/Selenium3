e068d8fa8e5bb45bc91c7ea790ca04729250c1df
SanchesQA <Sasha388951>
2017-10-25 15:58:10 +0200

    correct gecko variable

--- src/test/java/BeforeAllTests.java
+++ src/test/java/BeforeAllTests.java
@@ -5,11 +5,13 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+
 public class BeforeAllTests {
 
     @BeforeClass
     public static void setUp() throws InterruptedException{
-      //  System.setProperties("webdriver.gecko.driver", "C:\\Users\\agrebenkina\\IdeaProjects\\geckodriver-v0.19.0-win64\\geckodriver.exe");
+     //  System.setProperties("webdriver.gecko.driver", "C:\\Users\\agrebenkina\\IdeaProjects\\geckodriver-v0.19.0-win64\\geckodriver.exe");
+        System.setProperty("webdriver.gecko.driver", "C:\\Users\\agrebenkina\\IdeaProjects\\geckodriver-v0.19.0-win64\\geckodriver.exe");
         DesiredCapabilities capability =  DesiredCapabilities.firefox();
         capability.setCapability("marionette", true);
         WebDriver driver = new FirefoxDriver(capability);
