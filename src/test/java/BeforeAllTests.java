f198f83bb078113eeb1097fadcaa740ce853f577
SanchesQA <Sasha388951>
2017-10-30 14:59:49 +0100

    softAssert added

--- src/test/java/BeforeAllTests.java
+++ src/test/java/BeforeAllTests.java
@@ -1,27 +1,60 @@
 
+import javafx.scene.control.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.firefox.internal.ProfilesIni;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import java.util.concurrent.TimeUnit;
 
 
 public class BeforeAllTests {
+    public WebDriver driver;
 
-    @BeforeClass
-    public static void setUp() throws InterruptedException{
-     //  System.setProperties("webdriver.gecko.driver", "C:\\Users\\agrebenkina\\IdeaProjects\\geckodriver-v0.19.0-win64\\geckodriver.exe");
+    @BeforeMethod
+    public void setUp() throws InterruptedException{
         System.setProperty("webdriver.gecko.driver", "C:\\Users\\agrebenkina\\IdeaProjects\\geckodriver-v0.19.0-win64\\geckodriver.exe");
-        DesiredCapabilities capability =  DesiredCapabilities.firefox();
-        capability.setCapability("marionette", true);
-        WebDriver driver = new FirefoxDriver(capability);
-        driver.get("http://toolsqa.com/selenium-webdriver/how-to-use-geckodriver/");
-        Thread.sleep(1000);
-        driver.quit();
+     //   DesiredCapabilities capability =  DesiredCapabilities.firefox();
+     //   capability.setCapability("marionette", true);
+      //  driver = new FirefoxDriver(capability);
+        driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.get("https://games.williamhill.com/#!/");
+
+
 
     }
     @Test
-    public static void someTest(){
+    public  void airbnbTest(){
+        driver.manage().timeouts().implicitlyWait(10L, TimeUnit.SECONDS);
+        WebElement promotionsTile;
+        promotionsTile = driver.findElement(By.xpath("//a[@title='PROMOTIONS']"));
+        promotionsTile.click();
+        WebElement promoBlock;
+        promoBlock = driver.findElement(By.cssSelector(".promotion-tiles-list"));
+        SoftAssert s_ass = new SoftAssert();
+        s_ass.assertEquals(true, promotionsTile.isSelected());
+        System.out.println("Menu is Selected"+promotionsTile.isSelected());
+        Assert.assertEquals(true, promoBlock.isDisplayed());
+        s_ass.assertAll();
+
+
+
+
 
     }
+   @AfterMethod
+    public  void tearDown(){
+        driver.quit();
+    }
+
 }
